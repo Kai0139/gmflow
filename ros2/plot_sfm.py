068366b1d2d35ca94c6efa5f05f35b7a4ac8d425
Kai0139 <zkrs3914@outlook.com>
2024-06-02 20:16:52 +0800

    adjust aspect

--- ros2/plot_sfm.py
+++ ros2/plot_sfm.py
@@ -11,6 +11,9 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+def closest(lst, K):
+    return min(range(len(lst)), key = lambda i: abs(lst[i]-K))
+
 class FlowFromBag(object):
     def __init__(self) -> None:
         self.bag_path = Path('/home/viplab/data/thermal_slam_bags/ros2/sfm_xyz1')
@@ -21,11 +24,15 @@ class FlowFromBag(object):
         self.window_pose_topic = "/window_pose_array"
         self.window_gt_topic = "/gt_pose_array"
         self.point_marker_topic = "/point_marker"
-        self.topic_list = [self.window_pose_topic, self.point_marker_topic, self.window_gt_topic]
+        self.grad_img_topic = "/fsvio_ros2/grad_image"
+        self.topic_list = [self.window_pose_topic, self.point_marker_topic, self.window_gt_topic, self.grad_img_topic]
         
     def read_bag(self):
         point_sets = []
         pose_sets = []
+        sfm_ts_list = []
+        grad_img_list = []
+        grad_img_ts_list = []
         self.reader.open()
         connections = [x for x in self.reader.connections if x.topic in self.topic_list]
         for connection, timestamp, rawdata in self.reader.messages(connections=connections):
@@ -35,16 +42,31 @@ class FlowFromBag(object):
                 for pt in msg.points:
                     pts.append([pt.x, pt.y, pt.z])
                 point_sets.append(np.array(pts))
-            if connection.topic == self.window_pose_topic:
+                sfm_ts_list.append(timestamp)
+            elif connection.topic == self.window_pose_topic:
                 poses = []
                 for pose in msg.poses:
                     poses.append([pose.position.x, pose.position.y, pose.position.z, 
                                   pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z])
                 pose_sets.append(np.array(poses))
+            elif connection.topic == self.grad_img_topic:
+                grad_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+                grad_img_list.append(grad_img)
+                grad_img_ts_list.append(timestamp)
+
         for idx in range(len(point_sets)):
             # print(idx)
+            # self.plot_3d_points(point_sets[idx], pose_sets[idx])
             pass
-        self.plot_3d_points(point_sets[37], pose_sets[37])
+        # sfm1: 37
+        # sfm3: 20 60
+        # sfm4: 27 28 30 49
+        sfm_idx = 39
+        self.plot_3d_points(point_sets[sfm_idx], pose_sets[sfm_idx])
+        # print(sfm_ts_list[37])
+        # print(grad_img_ts_list[100])
+        closest_idx = closest(grad_img_ts_list, sfm_ts_list[sfm_idx])
+        cv2.imwrite("grad_img.png", grad_img_list[closest_idx-10])
         self.reader.close()
 
     def plot_3d_points(self, pts, poses):
@@ -73,8 +95,8 @@ class FlowFromBag(object):
         # Set labels and title
         ax.set_xlim3d([-1.2, 1.2])
         ax.set_ylim3d([0, 2.4])
-        ax.set_zlim3d([-1.2, 1.2])
-        ax.set_box_aspect((1, 1, 1))
+        ax.set_zlim3d([-0.8, 0.8])
+        ax.set_box_aspect((1, 1, 2/3))
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
